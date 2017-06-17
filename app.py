a3f7ee3c7458d136168604181674bdfc35b06bdf
shreyanshp <shreyansh.pandey@fractalanalytics.com>
2017-06-17 13:19:11 +0530

    changes

--- app.py
+++ app.py
@@ -9,19 +9,36 @@
 #requests are objects that flask handles (get set post, etc)
 from flask import Flask, render_template,request, current_app, jsonify
 import io
-import model
 import base64
 import re
+import keras
+from keras.preprocessing import image
+from keras.applications.inception_v3 import preprocess_input, decode_predictions
+import numpy as np
+import tensorflow as tf
+
+model = keras.applications.inception_v3.InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
+graph = tf.get_default_graph()
+
+
+def mypredict(image_file):
+    img = image.load_img(image_file)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x,axis=0)
+    x = preprocess_input(x)
+
+    global graph
+    with graph.as_default():
+        preds = model.predict(x)
+
+    top3 = decode_predictions(preds,top=3)[0]
+
+    predictions = [{'label': label, 'description': description, 'probability': probability * 100.0}
+                    for label,description, probability in top3]
+    return predictions
+
 #initalize our flask app
 app = Flask(__name__)
-
-#decoding an image from base64 into raw representation
-def convertImage(imgData1):
-	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
-	#print(imgstr)
-	with open('output.png','wb') as output:
-		output.write(imgstr.decode('base64'))
-
 
 @app.route('/')
 def index():
@@ -41,7 +58,7 @@ def predict():
 
 	data = base64.b64decode(data)
 	image = io.BytesIO(data)
-	predictions = model.predict(image)
+	predictions = mypredict(image)
 	current_app.logger.info('Predictions: %s', predictions)
 	return jsonify(predictions=predictions)
 
